86a43c9f4bcc75c5956069df28408f9a9b14cbad
Notker367 <notstrauss@gmail.com>
2025-01-05 15:54:07 +0400

    v0.0.3 site prototype

--- docs/app.js
+++ docs/app.js
@@ -1,7 +1,6 @@
-document.querySelectorAll('.expand-button').forEach(button => {
+document.querySelectorAll('.footer-line').forEach(button => {
     button.addEventListener('click', () => {
         const action = button.dataset.tgAction;
-        console.log(`Mock action triggered: ${action}`);
         if (action === 'showDailyMessage') {
             alert('Показать историю сообщений!');
         } else if (action === 'showTasks') {
